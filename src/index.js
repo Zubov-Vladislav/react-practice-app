4ec2ba46a7ac82157628a031a3ae1d39aff00ffa
Vladislav <vladislav.exceedteam@gmail.com>
2020-04-27 12:14:14 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
+﻿import React from "react";
 import ReactDOM from "react-dom";
 import "./index.module.css";
-import App from "./App";
+import App from ‘./App’;
 import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 
