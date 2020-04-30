1b050db56a2600e1119a55403dcbb3de41e1a354
Alex <57138526+alexnikitin341@users.noreply.github.com>
2020-04-30 17:04:13 +0300

    added Loaded and card trouth Id

--- src/store/reducers/products.js
+++ src/store/reducers/products.js
@@ -1,8 +1,11 @@
+import axios from "axios";
+
 export function getProductsList(store) {
   return store.products.list.map((product) => product.id);
 }
+
 const initialState = {
-  isLoading: false,
+  isLoading: true,
   list: [],
 };
 export function getProductById(store, productId) {
@@ -11,10 +14,24 @@ export function getProductById(store, productId) {
 
 export function reducer(state = initialState, action) {
   switch (action.type) {
-    case "ADD_PRODUCTS_TO_LIST": {
+    case "ADD_PRODUCTS_TO_LIST_START": {
+      return {
+        ...state,
+        isLoading: true,
+      };
+    }
+    case "ADD_PRODUCTS_TO_LIST_SUCCESS": {
       return {
         ...state,
         list: [...state.list, ...action.products],
+        isLoading: false,
+      };
+    }
+    case "ADD_PRODUCTS_TO_LIST_ERROR": {
+      return {
+        ...state,
+        list: [...state.list, ...action.products],
+        isLoading: false,
       };
     }
     default:
@@ -22,9 +39,27 @@ export function reducer(state = initialState, action) {
   }
 }
 
-export function addProductsToList(products) {
-  return {
-    type: "ADD_PRODUCTS_TO_LIST",
-    products,
+export function addProductsToList() {
+  return (dispatch) => {
+    dispatch({
+      type: "ADD_PRODUCTS_TO_LIST_START",
+    });
+
+    return axios
+      .get("https://simplereactshop.herokuapp.com/api/products")
+      .then((response) => {
+        const cards = [...response.data.products];
+        return dispatch({
+          type: "ADD_PRODUCTS_TO_LIST_SUCCESS",
+          products: cards,
+        });
+      })
+
+      .catch((error) => {
+        return dispatch({
+          type: "ADD_PRODUCTS_TO_LIST_ERROR",
+          error,
+        });
+      });
   };
 }
